a033cbb7f80d753fa7e5a4325217d8cb37045b2a
osakaruhub <teddyfreddy10@gmail.com>
2024-03-30 11:34:28 +0100

    added functionalities
    added option for vlc
    added configuration
    you can now search with every character

--- README.md
+++ README.md
@@ -1,23 +1,33 @@
 # rofi-youtube
 
-search for youtube content using the rofi frontend
-searching for youtube videos that you already know the title in the website may seem clunky, and slow sometimes. rofi-youtube aims to be a fast frontend for searching youtube links.
+<p>search for youtube content using the rofi frontend<br>
+searching for youtube videos that you already know the title in the website may seem clunky, and slow sometimes. rofi-youtube aims to be a fast frontend for searching youtube links.</p>
 
 # Features
 
 - search for any youtube video
 - watch from mpv (soon more!) or just listen
 
-# Installing
+# Installation
 
 Download the rofi-youtube executable and run it, it's as easy as that.
 
+# Configuration
+
+The default path for the configuration file is in ~/.config/rofi-youtube/config.ini.<br>
+possible condifguration:
+| field | value |
+|--------------- | --------------- |
+| api_key | N/A |
+| default_player | mpv |
+| rofi_config_path | ~/.config/rofi |
+
 # Dependincies
 
-[rofi](https://github.com/davatorium/rofi) (awesome menu dialog, dmenu alternative)
+[rofi](https://github.com/davatorium/rofi) (awesome menu dialog, dmenu alternative)<br>
 [mpv](https://github.com/mpv-player/mpv) (simple light-weight multimedia player)
 
 # Contributors
 
-just me for the moment.
-feel free to contribute though.
+<p>just me for the moment.<br>
+feel free to contribute though.</p>
--- rofi-youtube.sh
+++ rofi-youtube.sh
@@ -1,20 +1,27 @@
-if [ ! -f ~/.config/rofi-youtube/config.ini ]; then
+rofi_youtube_path=~/.config/rofi-youtube/config.ini
+rofi_config_path=~/.config/rofi/config.rasi
+
+
+if [[ -f "$rofi_youtube_path" ]]; then
+  . $rofi_youtube_path
+else
   mkdir ~/.config/rofi-youtube
-  touch ~/.config/rofi-youtube/config.ini
-  echo "no configuration file found!\ncreated configuration file at ~/.config/rofi-youtube/config.ini"
+  cp /etc/rofi_config.ini $rofi_youtube_path
+  rofi -c rofi_config_path -dmenu -mesg "no configuration file found!\ncreated configuration file at $rofi_youtube_path"
   exit 0;
 fi
 
 no_video=false
-# api_key=$(grep -E '^api-key\s*=' ~/.config/rofi-youtube/config.ini | sed -E 's/^api-key\s*=\s*//' | awk -F '=' '{print $2}' | tr -d ' ')
-api_key=$(cat ~/.config/rofi-youtube/config.ini)
+verbose=false
 
 if [[ -z api_key ]]; then
   echo "no API Key found"
+  api_key = $(rofi -c $rofi_config_path --pasword -p "set API Key")
+  sed -i "s/api_key = .*/api_key = $api_key/" $rofi_youtube_path
   exit 0;
 fi
 
-while getopts ":h:v:-n"; do
+while getopts ":h:v:-V:-n:c:"; do
   case "option" in
     h)
       printf "%s\n"
@@ -23,8 +30,10 @@ while getopts ":h:v:-n"; do
 		    	   "rofi frontend for youtube - search and play youtube content using rofi!\n"
 		    	   "\n"
 		    	   "Posible command options:\n"
-		    	   "		        -h | --help       : Display this message\n"
-             "		        -v | --version    : Print version and exit\n"
+		    	   "		        -h | --help     : Display this message\n"
+             "		        -v | --version  : Print version and exit\n"
+             "            -c | --config   : Edit the configuration file"
+             "            -V | --verbose  : Debug output"
 		    	   "\n";
 		  exit 0;
       ;;
@@ -32,8 +41,18 @@ while getopts ":h:v:-n"; do
       printf $RELEASE_VERSION
       return 0;
       ;;
+    V | --verbose)
+      verbose=true
+      ;;
     n | --no-video)
       no_video=true
+      ;;
+    c)
+      if [ -x "(command -v nano)" ]; then
+        nano $rofi_youtube_path
+      else
+        vim $rofi_youtube_path
+      fi
       ;;
     *)
       printf "invalid option"
@@ -42,11 +61,27 @@ while getopts ":h:v:-n"; do
   esac
 done
 
-searchTerm=$(rofi -dmenu -mesg "search for video")
+searchTerm=$(rofi -c $rofi_config_path -dmenu -mesg "search for video")
+# make search readable for youtube
+searchTerm=$(echo "$searchTerm" | sed 's/ /%20/g; s/!/ %21/g; s/*/ %2A/g; s/'\''/ %27/g; s/(/ %28/g; s/)/ %29/g; s/;/ %3B/g; s/:/ %3A/g; s/@/ %40/g; s/&/ %26/g; s/=/ %3D/g; s/+/ %2B/g; s/\$/ %24/g; s/,/ %2C/g; s/\// %2F/g; s/?/ %3F/g; s/#/ %23/g; s/\[/ %5B/g; s/]/ %5D/g;')
 URL="https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="$searchTerm"&type=video&key="$api_key
+if [[ $verbose=true ]]; then
+  echo "$URL"
+fi
 response=$(curl "$URL")
 
-title=$(jq -r '.items[].snippet.title' <<< "$response" | rofi -dmenu)
+title=$(jq -r '.items[].snippet.title' <<< "$response" | rofi -c $rofi_config_path -dmenu)
 videoId=$(jq --arg title "$title" -r '.items[] | select(.snippet.title == $title).id.videoId' <<< "$response")
+if [[ $verbose = true ]]; then
+  echo "videoID: $videoId"
+fi
 
-mpv "https://www.youtube.com/watch?v=$videoId"
+videoBool=""
+if [ "$no_video" = true ]; then
+  videoBool="--no-video"
+fi
+if [[ -z "$default_player" ]]; then
+  mpv $videoBool "https://www.youtube.com/watch?v=$videoId"
+else
+  eval $default_player $videoBool "https://www.youtube.com/watch?v=$videoId"
+fi
