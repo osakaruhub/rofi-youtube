4cb855902bd3e2733265978d645f1714ede9b1be
osakaruhub <teddyfreddy10@gmail.com>
2024-04-03 12:22:27 +0200

    jq request error show fix

--- rofi-youtube.sh
+++ rofi-youtube.sh
@@ -70,8 +70,8 @@ searchTerm=$(rofi -c $rofi_config -dmenu -mesg "search for video")
 # make search readable for youtube
 searchTerm=$(echo "$searchTerm" | sed 's/ /%20/g; s/!/ %21/g; s/*/ %2A/g; s/'\''/ %27/g; s/(/ %28/g; s/)/ %29/g; s/;/ %3B/g; s/:/ %3A/g; s/@/ %40/g; s/&/ %26/g; s/=/ %3D/g; s/+/ %2B/g; s/\$/ %24/g; s/,/ %2C/g; s/\// %2F/g; s/?/ %3F/g; s/#/ %23/g; s/\[/ %5B/g; s/]/ %5D/g;')
 URL="https://youtube.googleapis.com/youtube/v3/search?part=snippet&q="$searchTerm"&type=video&key="$api_key
-response=$(curl "$URL")
-if [[ jq -r '.error' <<< "$response" > /dev/null ]] ; then
+response=$(curl -s "$URL")
+if jq -r '.error' <<< "$response" &> /dev/null = "" ; then
   rofi -c $rofi_config -dmenu -mesg "$(printf $response | jq '.error.message')"
 fi
 
